5313e18b625fe34eb4b971bf75734734a4f48c45
ABexcalibur <87257587+ABexcalibur@users.noreply.github.com>
2021-08-13 17:31:33 +0530

    new

--- src/main/java/com/athlima/bots/Main.java
+++ src/main/java/com/athlima/bots/Main.java
@@ -1,16 +1,7 @@
 package com.athlima.bots;
 
-import com.athlima.bots.listeners.Announce;
-import com.athlima.bots.listeners.Help;
-import com.athlima.bots.listeners.Nickname;
-import com.athlima.bots.listeners.Ping;
 import org.javacord.api.DiscordApi;
 import org.javacord.api.DiscordApiBuilder;
-import org.javacord.api.util.event.ListenerManager;
-
-import java.util.ListIterator;
-import java.util.Optional;
-import java.util.concurrent.TimeUnit;
 
 public class Main {
     public static final String prefix = "ath ";
@@ -20,6 +11,6 @@ public class Main {
                 .setToken(System.getenv("TOKEN"))
                 .login().join();
         AddListeners addListeners = new AddListeners(api);
-        System.out.println("Bot is online!");
+        System.out.println("Javacord is online!");
     }
 }
